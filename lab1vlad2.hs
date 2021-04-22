06add025b1801620b870603a83504a8fd88a9c4c
PaulVLAD22 <paulvlad34@gmail.com>
2021-04-22 16:56:56 +0300

    .

--- lab1vlad2.hs
+++ lab1vlad2.hs
@@ -1,38 +1,28 @@
-import Data.Maybe ( fromMaybe )
 data Prog = On Instr
-
-data Instr = Off
-    | Expr :> Instr
-
-data Expr = Mem
-    | V Int
-    | Expr :+ Expr
-
+data Instr = Off | Expr :> Instr
+data Expr = Mem | V Int | Expr :+ Expr
 type Env = Int -- valoarea celulei de memorie
 type DomProg = [Int]
 type DomInstr = Env -> [Int]
 type DomExpr = Env -> Int
 
 prog :: Prog -> DomProg
-prog (On s) = stmt s 0
+prog (On instr) = []
 
 stmt :: Instr -> DomInstr
-stmt Off _= []
-stmt (e :> a) m =expr e m : stmt a (expr e m)
-
-
+stmt Off env = []
+stmt (e :> instr) env = expr e env : stmt instr (expr e env)
 
 expr :: Expr -> DomExpr
-expr (V a) _ = a
-expr Mem b = b
-expr (e1 :+ e2 ) m = expr e1 m + expr e2 m
+expr Mem env = env
+expr (V i) env = i
+expr (t1 :+ t2) env = expr t1 env + expr t2 env
 
 
--- ce n ai stiut: mereu trebuie sa tii undeva valoarea env si sa gandesti unde ii e locu
--- citeste mai bine cerinta
+t=On ((V 3) :> ((Mem :+ (V 5)) :> Off))
+
 
 type Name = String
-
 data Hask = HTrue
     | HFalse
     | HLit Int
@@ -54,42 +44,40 @@ data Value = VBool Bool
     | VError -- pentru reprezentarea erorilor
 
 type HEnv = [(Name, Value)]
-
 type DomHask = HEnv -> Value
 
 instance Show Value where
+    show (VInt i) = show i 
+    show (VFun f) = "Function"
+    show VError = "Error"
     show (VBool b) = show b
-    show (VInt i )= show i
-    show (VFun f) = show "Functie"
-    show (VError) = show "Error"
-    
-instance Eq Value where
-    VBool a == VBool b = a==b
+
+instance Eq Value where 
     VInt i == VInt j = i==j
-    _ == _  = error " Nu se pot compara "
+    VBool b1 == VBool b2 = b1 == b2
+    VFun f == VFun f2 = error "Functions"
+    VError == VError = error "Error"
+    _ == _ = error "different types not comparable"
 
 hEval :: Hask -> DomHask
 
 hEval HTrue env = VBool True 
 hEval HFalse env = VBool False 
-hEval (HLit i) env = VInt i
-hEval (HIf h1 h2 h3) env = case hEval h1 env of
-    VBool True -> hEval(h2) env
-    VBool False -> hEval(h3) env
-hEval (h1 :==: h2) env = case  hEval h1 env == hEval h2 env of
-    True -> VBool True 
-    False -> VBool False 
+hEval (HLit i) env = VInt i 
+hEval (HIf h1 h2 h3) env = case hEval h1 env of 
+    VBool True -> hEval h2 env 
+    VBool False -> hEval h3 env
+hEval (h1 :==: h2) env = VBool (hEval h1 env == hEval h2 env)
 hEval (d :+: e) r 	=	hadd (hEval d r) (hEval e r)
 	where 	
 		hadd (VInt i) (VInt j) = VInt (i + j)
 		hadd _ _ = VError
-
-hEval (HVar x) r = fromMaybe VError (lookup x r)
-
+hEval (HVar n) env = case lookup n env of 
+    Nothing -> VError
+    Just x -> x
 hEval (HLam x e ) r = VFun (\v -> hEval e ((x,v):r))
 
 
 
--- 2
 
 
